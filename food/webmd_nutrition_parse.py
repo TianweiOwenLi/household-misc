094da7d5442e4e9267c90f94215310ea5c69dbb6
Owen <tianwei2@andrew.cmu.edu>
2023-05-21 02:56:21 -0400

    added nutritional fact dataclass

--- food/webmd_nutrition_parse.py
+++ food/webmd_nutrition_parse.py
@@ -1,7 +1,33 @@
+from dataclasses import dataclass
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import re
+
+@dataclass
+class nutritions:
+  """nutritional facts"""
+
+  # proteins, carbs, and lipids are in grams.
+  protein: int = 0
+  carb: int = 0
+  lipid: int = 0
+
+  # vitamins, minerals, and dietary fibers are in mg.
+  vitamin_a: int = 0
+  vitamin_b3: int = 0
+  vitamin_b6: int = 0
+  vitamin_b12: int = 0
+  vitamin_c: int = 0
+  vitamin_d: int = 0
+  vitamin_e: int = 0
+
+  sodium: int = 0
+  potassium: int = 0
+
+  dietary_fiber: int = 0
+
 
 def scrape_href_list(driver, url, patt):
   """ Given some `selenium` driver, an url, and some regex pattern, 
@@ -17,7 +43,6 @@ def scrape_href_list(driver, url, patt):
       href_list.append(link)
   
   return href_list
-
 
 
 driver = webdriver.Firefox()
