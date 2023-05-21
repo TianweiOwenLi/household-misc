c5ba7e158298e6590f43f9bc031a7a27678d9129
Owen <tianwei2@andrew.cmu.edu>
2023-05-21 03:12:42 -0400

    Refactored tags into separate vars; re-organized nutritions fields

--- food/webmd_nutrition_parse.py
+++ food/webmd_nutrition_parse.py
@@ -5,16 +5,29 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import re
 
+# start of food-category page url
+food_category_patt = r"https://www.webmd.com/diet/ingredients-guide/"
+
+# start of food-health-benefit page url
+food_patt = r"https://www.webmd.com/diet/health-benefits-"
+
+# html element class name of food serving portion
+serving_cls_name = "nutrition-col__serving-value"
+
+# html element class name of food macro-nutrition portion
+macro_nutrition_cls_name = "macro-table__thead"
+
 @dataclass
 class nutritions:
   """nutritional facts"""
 
-  # proteins, carbs, and lipids are in grams.
+  # proteins, carbs, lipids, and dietary fiber are in grams.
   protein: int = 0
   carb: int = 0
   lipid: int = 0
+  dietary_fiber: int = 0
 
-  # vitamins, minerals, and dietary fibers are in mg.
+  # vitamins and minerals are in mg.
   vitamin_a: int = 0
   vitamin_b3: int = 0
   vitamin_b6: int = 0
@@ -25,8 +38,6 @@ class nutritions:
 
   sodium: int = 0
   potassium: int = 0
-
-  dietary_fiber: int = 0
 
 
 def scrape_href_list(driver, url, patt):
@@ -45,16 +56,24 @@ def scrape_href_list(driver, url, patt):
   return href_list
 
 
+def scrape_nutritional_facts(driver, url):
+  """ Given some `selenium` driver and an url to WebMD food benefit page, 
+  returns TODO
+  """
+  driver.get(url)
+
+  portion = driver.find_element(By.CLASS_NAME, serving_cls_name).text
+
+
+
 driver = webdriver.Firefox()
 webmd_url = "https://www.webmd.com/diet/ingredients-guide"
 result = {}
 
 # obtain links to each food category, i.e. fruits, vegetables, ...
-food_category_patt = r"https://www.webmd.com/diet/ingredients-guide/"
 food_category_links = scrape_href_list(driver, webmd_url, food_category_patt)
 
 # obtain links to each individual food
-food_patt = r"https://www.webmd.com/diet/health-benefits-"
 food_links = []
 for food_cat_link in food_category_links:
   food_links.extend(scrape_href_list(driver, food_cat_link, food_patt))
